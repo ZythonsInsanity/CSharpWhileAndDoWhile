aee00c3bc7b6fd89f6096e4173066ca6050a7286
ZythonsInsanity <37195840+ZythonsInsanity@users.noreply.github.com>
2018-08-01 14:03:21 -0700

    Uploaded 2 Assignments for Repository

---
+++ booldowhileLoop/booldowhileLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+    class Program
+    {
+    static void Main()
+    {
+        Console.WriteLine("What year was I born? Hint: I'm a mid-late millenial.");
+        int yearBorn = Convert.ToInt32(Console.ReadLine());
+        bool yearGuess = yearBorn == 1989;
+
+        do
+            switch (yearBorn)
+            {
+                case 1986:
+                    Console.WriteLine("Nope not 1986! Try again :)");
+                    Console.WriteLine("What year was I born? Hint: I'm a mid-late millenial.");
+                    yearBorn = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 1987:
+                    Console.WriteLine("Sorry not 1987. Getting close though! Try again :)");
+                    Console.WriteLine("What year was I born? Hint: I'm a mid-late millenial.");
+                    yearBorn = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 1988:
+                    Console.WriteLine("Nope! Almost got it!! Try again :)");
+                    Console.WriteLine("What year was I born? Hint: I'm a mid-late millenial.");
+                    yearBorn = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 1989:
+                    Console.WriteLine("Yes! I was born in 1989! Ready for all the fun the 90's had to provide! :)");
+                    yearGuess = true;
+                    break;
+                default:
+                    Console.WriteLine("That's not it... Try again! :)");
+                    Console.WriteLine("What year was I born? Hint: I'm a mid-late millenial.");
+                    yearBorn = Convert.ToInt32(Console.ReadLine());
+                    break;
+            }
+        while (!yearGuess);
+        Console.ReadLine();
+        }
+    }
+
